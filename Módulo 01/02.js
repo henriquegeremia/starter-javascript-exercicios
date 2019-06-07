a2f53d91bb815d18875589a1cfb21dd546af29a3
Henrique Geremia Nievinski <henriquegeremia@gmail.com>
2019-06-07 09:21:40 -0300

    solução alternativa
    testa se começa com par e depois só itera somando 2.  (obrigado pelo conteúdo de qualidade e free!)

--- Módulo 01/02.js
+++ Módulo 01/02.js
@@ -7,3 +7,15 @@ function pares(x, y) {
 }
 
 pares(32, 321);
+
+
+// alternativa
+
+function pares(x, y) {
+    z = (x%2 == 0) ? x : ++x;
+  
+    while (z < y) {
+        console.log(z);
+        z += 2;
+    } 
+}
